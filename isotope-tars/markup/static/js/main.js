32e9cae94d185eec50119a2bc86d30780d872b36
gearmobile <gearmobile@gmail.com>
2016-05-25 15:43:27 +0300

    fuck

---
+++ isotope-tars/markup/static/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const $ = require('jquery');
+const Isotope = require('isotope-layout');
+const imagesLoaded = require('imagesloaded');
+imagesLoaded.makeJQueryPlugin( $ );
+
+
+// ISOTOPE LAYOUT
+// -----------------------------------------------------------
+
+var $iso1 = new Isotope( '.iso1', {
+    itemSelector: '.iso1 .grid-item',
+    percentPosition: true,
+    masonry: {
+        columnWidth: '.iso1 .grid-sizer'
+    }
+});
+
+
+// IMAGELOADED
+// -----------------------------------------------------------
+
+
+
+var $iso2 = new Isotope( '.iso2', {
+    itemSelector: '.iso2 .grid-item',
+    percentPosition: true,
+    masonry: {
+        columnWidth: '.iso2 .grid-sizer'
+    }
+});
+
