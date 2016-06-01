c5d8004c15e5ac52ebc5f40eca9ae11caf1d26dc
gearmobile <gearmobile@gmail.com>
2016-06-02 00:29:06 +0300

    webpack-plugins

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,20 @@
 "use strict";
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
     entry: './home.js',
     output: {
         filename: './build.js',
         library: 'home'
     },
-    watch: true,
+    watch: NODE_ENV === 'development',
     watchOptions: {
         aggregateTimeout: 100
     },
-    devtool: 'cheap-inline-module-source-map',
-    devtool: 'eval'
+    devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : null,
+    plugins: [
+        new webpack.EnvironmentPlugin
+    ]
 };
