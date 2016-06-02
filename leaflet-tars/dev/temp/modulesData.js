fb50352aa4b529dfd3c32e94533788833fde7b46
gearmobile <gearmobile@gmail.com>
2016-06-02 08:10:58 +0300

    leaflet-tars

---
+++ leaflet-tars/dev/temp/modulesData.js
@@ -0,0 +1,30 @@
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data map */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+}
+,
+
+head: {
+    defaults: {
+        title: 'default title',
+        useSocialMetaTags: true
+    }
+},
+
+__pages: [{
+                name: 'index',
+                href: 'index.html'
+             }]
