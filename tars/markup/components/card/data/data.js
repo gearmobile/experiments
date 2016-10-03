095fea8b919711a95cfd64e115aeb5a151bb3eea
gearmobile <gearmobile@gmail.com>
2016-10-03 14:27:03 +0300

    tars

---
+++ tars/markup/components/card/data/data.js
@@ -0,0 +1,20 @@
+var data = { card: {
+    users: {
+        john: { // item
+            name: 'John',
+            profession: 'Cooker'
+        },
+        james: { // item
+            name: 'James',
+            profession: 'Manager'
+        },
+        joseph: { // item
+            name: 'Joseph',
+            profession: 'Director'
+        },
+        jeremy: {  // item
+            name: 'Jeremy',
+            profession: 'Sailor'
+        },
+    }
+}};
