a495b68356147b3f1934b3ed0ece0c1185706ae5
gearmobile <gearmobile@gmail.com>
2016-12-22 16:12:21 +0300

    skyapp - app.js

--- bootstrap-3/skyapp/js/app.js
+++ bootstrap-3/skyapp/js/app.js
@@ -1,5 +1,23 @@
-// navbar change active link
-$( '.nav a' ).on( 'click', function () {
-    $( '.nav' ).find( '.active' ).removeClass( 'active' );
-    $( this ).parent().addClass( 'active' );
-});
+
+// HEADER-MAIN CHANGE ACTIVE LINK CLASS
+// --------------------------------------------
+
+// $( '.header-main__primary li > a' ).on( 'click', function () {
+//     $( '.header-main__primary' ).find( '.active' ).removeClass( 'active' );
+//     $( this ).parent().addClass( 'active' );
+// });
+
+
+// $( '.header-main__primary > li > a' ).on( 'click', function () {
+//     $( '.header-main__primary > li' ).filter( '.active' ).removeClass( 'active' );
+// });
+
+
+window.addEventListener( 'load', function () {
+    //
+}, false );
+
+
+
+// console.log( document.querySelectorAll( '.header-main__primary li' ) );
+
