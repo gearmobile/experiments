c5f95f52ad9b0997831e0ffb9b5fecdb37775b83
gearmobile <gearmobile@gmail.com>
2016-12-23 16:08:22 +0300

    scrollmagic

--- scrollmagic/case-01/app.js
+++ scrollmagic/case-01/app.js
@@ -1,3 +1,12 @@
-window.addEventListener('DOMContentLoaded', function () {
-    //
+window.addEventListener( 'load', function () {
+    // --------------------------------------------------
+    const blockParallax = document.querySelector( '.parallax' );
+    const blockContent = document.querySelector( '.parallax__secondary' );
+    // --------------------------------------------------
+    const parallax = new ScrollMagic.Scene({
+            triggerElement: blockParallax,
+            triggerHook: 1,
+            duration: '200%'
+        }).setTween( TweenMax.from( blockContent, 1, { y: '-40%' } ) ).addTo( controller );
+    // --------------------------------------------------
 }, false);
