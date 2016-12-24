af9c96606772775e33f98d3f85205a07d4b4ecbb
gearmobile <gearmobile@gmail.com>
2016-12-24 14:36:48 +0300

    skyapp

--- bootstrap-3/skyapp/js/app.js
+++ bootstrap-3/skyapp/js/app.js
@@ -1,7 +1,3 @@
-
-$( document ).ready( function () {
-    $( ".navbar-default .navbar-nav > li > a" ).on("click", function(){
-        $( ".navbar-default .navbar-nav > li" ).find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });
+$( document ).ready( function() {
+    $( '.block-equal' ).matchHeight();
 });
