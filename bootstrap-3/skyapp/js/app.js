6b5bc9d21c2aece34a2bd36e3bfc975e59022aff
gearmobile <gearmobile@gmail.com>
2016-12-24 17:02:54 +0300

    skyapp

--- bootstrap-3/skyapp/js/app.js
+++ bootstrap-3/skyapp/js/app.js
@@ -1,3 +1,25 @@
 $( document ).ready( function() {
     $( '.block-equal' ).matchHeight();
+    // ---------------------------------
+    // $('ul.nav > li').click(function (e) {
+    //     e.preventDefault();
+    //     $('ul.nav > li').removeClass('active');
+    //     $(this).addClass('active');
+    // });
+    $( '.header-main__primary a' ).on( 'click', function(){
+        // console.log( $(this).attr( 'href' ) );
+        // $(this).parent().addClass( 'active' );
+        // alert( 'Click' );
+        // var this = $(this);
+        // $( '.header-main__primary' ).find( '.active' ).removeClass( 'active' );
+        // $( this ).parent().addClass( 'active' );
+    });
 });
+
+// $(document).ready(function () {
+//     $('ul.nav > li').click(function (e) {
+//         e.preventDefault();
+//         $('ul.nav > li').removeClass('active');
+//         $(this).addClass('active');
+//     });
+// });
