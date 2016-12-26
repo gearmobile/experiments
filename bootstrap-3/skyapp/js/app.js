139fa7f4d9763266fda11b553a047a9cf6d82cc7
gearmobile <gearmobile@gmail.com>
2016-12-26 15:30:28 +0300

    skyapp

--- bootstrap-3/skyapp/js/app.js
+++ bootstrap-3/skyapp/js/app.js
@@ -1,29 +1,11 @@
 $( document ).ready( function() {
-    $( '.block-equal' ).matchHeight();
-    // ---------------------------------
-    // $('ul.nav > li').click(function (e) {
-    //     e.preventDefault();
-    //     $('ul.nav > li').removeClass('active');
-    //     $(this).addClass('active');
-    // });
-    // $( '.header-main__primary a' ).on( 'click', function(){
-        // console.log( $(this).attr( 'href' ) );
-        // $(this).parent().addClass( 'active' );
-        // alert( 'Click' );
-        // var this = $(this);
-        // $( '.header-main__primary' ).find( '.active' ).removeClass( 'active' );
-        // $( this ).parent().addClass( 'active' );
-    // });
-    // Highlight the active nav link.
-    var url = window.location.pathname;
-    var filename = url.substr(url.lastIndexOf('/') + 1);
-    $('.navbar a[href$="' + filename + '"]').parent().addClass("active").siblings().removeClass('active');
-});
 
-// $(document).ready(function () {
-//     $('ul.nav > li').click(function (e) {
-//         e.preventDefault();
-//         $('ul.nav > li').removeClass('active');
-//         $(this).addClass('active');
-//     });
-// });
+    // BLOCK EQUAL HEIGHT
+    $( '.block-equal' ).matchHeight();
+
+    // HIGHLIGHT THE ACTIVE NAV LINK
+    var url = window.location.pathname;
+    var filename = url.substr(url.lastIndexOf( '/' ) + 1);
+    $( '.navbar a[href$="' + filename + '"]').parent().addClass( 'active' ).siblings().removeClass( 'active' );
+
+});
