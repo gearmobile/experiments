3c6ccc5ab3ebd7da50bec84e1e334a94fc246b86
gearmobile <gearmobile@gmail.com>
2016-12-26 19:14:29 +0300

    skyapp - page services small corrections

--- bootstrap-3/skyapp/js/app.js
+++ bootstrap-3/skyapp/js/app.js
@@ -4,8 +4,8 @@ $( document ).ready( function() {
     $( '.block-equal' ).matchHeight();
 
     // HIGHLIGHT THE ACTIVE NAV LINK
-    var url = window.location.pathname;
-    var filename = url.substr(url.lastIndexOf( '/' ) + 1);
-    $( '.navbar a[href$="' + filename + '"]').parent().addClass( 'active' ).siblings().removeClass( 'active' );
-
+    // var url = window.location.pathname;
+    // var filename = url.substr(url.lastIndexOf( '/' ) + 1);
+    // // $( '.navbar a[href$="' + filename + '"]').parent( 'li' ).siblings().removeClass( 'active' );
+    // $( '.navbar a[href$="' + filename + '"]').parent( 'li' ).addClass( 'active' );
 });
