5859cd392c723164f0aa2a39069496bd90d0998f
gearmobile <gearmobile@gmail.com>
2016-12-27 14:08:55 +0300

    vue.js - components

---
+++ vue.js/case-05/app.js
@@ -0,0 +1,35 @@
+// https://ru.vuejs.org/v2/guide/
+// https://vuejs.org/v2/guide/migration.html#Fragment-Instances-removed
+
+// РЕГИСТРАЦИЯ ГЛОБАЛЬНОГО КОМПОНЕНТА
+Vue.component( 'counter', {
+    template: '<h1>Hello World</h1>'
+});
+
+// РЕГИСТРАЦИЯ ГЛОБАЛЬНОГО КОМПОНЕНТА
+Vue.component( 'dropdown', {
+    template: '<ul class="nav">' +
+    '<li class="nav__item"><a class="nav__link" href="#">nav-1</a></li>' +
+    '<li class="nav__item"><a class="nav__link" href="#">nav-2</a></li>' +
+    '<li class="nav__item"><a class="nav__link" href="#">nav-3</a></li>' +
+    '<li class="nav__item"><a class="nav__link" href="#">nav-4</a></li>' +
+    '</ul>'
+});
+
+// РЕГИСТРАЦИЯ ГЛОБАЛЬНОГО КОМПОНЕНТА
+Vue.component( 'headling', {
+    template: '#counterTemplate'
+});
+
+var child = {
+    template: '<div>Пользовательский компонент!</div>'
+};
+
+// INSTANCE
+new Vue({
+    el: '#app',
+    components: {
+        // этот компонент будет доступен только внутри данного инстанса
+        'child-component': child
+    }
+});
