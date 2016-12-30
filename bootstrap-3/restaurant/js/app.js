e672c7c97d0f715124b449ed817f39b812077a6d
gearmobile <gearmobile@gmail.com>
2016-12-30 10:13:47 +0300

    res - add to top button

--- bootstrap-3/restaurant/js/app.js
+++ bootstrap-3/restaurant/js/app.js
@@ -4,3 +4,25 @@ $( document ).ready( function() {
     $( '.block-equal' ).matchHeight();
 
 });
+
+$(document).ready( function () {
+
+    var backToTop = $( '#back-to-top' );
+
+    $(window).scroll( function () {
+        if ( $( this ).scrollTop() > 50 ) {
+            $( backToTop ).fadeIn();
+        } else {
+            $( backToTop ).fadeOut();
+        }
+    });
+
+    // scroll body to 0px on click
+    backToTop.click( function () {
+        backToTop.tooltip( 'hide' );
+        $( 'body, html' ).animate({ scrollTop: 0 }, 800 );
+        return false;
+    });
+    backToTop.tooltip( 'show' );
+
+});
