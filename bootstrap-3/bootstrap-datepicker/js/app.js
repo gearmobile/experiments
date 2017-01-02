cea255ce1523151774006e29db670c7bfd0c7abc
gearmobile <gearmobile@gmail.com>
2017-01-02 12:09:29 +0300

    bootstrap-datepicker - fix show current date as selected

--- bootstrap-3/bootstrap-datepicker/js/app.js
+++ bootstrap-3/bootstrap-datepicker/js/app.js
@@ -1,20 +1,48 @@
 $( document ).ready( function () {
 
-    // SIMPLE
-    $( '#sandbox-container input' ).datepicker({
-        autoclose: true,
-        orientation: 'auto top',
-        todayHighlight: true,
-        container: '#sandbox-container'
-    });
+    var date = new Date();
+    var today = new Date( date.getFullYear(), date.getMonth(), date.getDate() );
 
-    // COMPONENT
-    $( '#datePicker' ).datepicker({
-        format: 'mm/dd/yyyy',
+    var optSimple = {
+        format: 'mm-dd-yyyy',
+        todayHighlight: true,
+        orientation: 'bottom right',
+        autoclose: true,
+        container: '#sandbox'
+    };
+
+    var optComponent = {
+        format: 'mm-dd-yyyy',
         container: '#datePicker',
         orientation: 'auto top',
         todayHighlight: true,
         autoclose: true
+    };
+
+    // SIMPLE
+    $( '#simple' ).datepicker( optSimple );
+
+    // COMPONENT
+    $( '#datePicker' ).datepicker( optComponent );
+
+    // ======================================================
+
+    $( '#datepicker1' ).datepicker({
+        format: "mm : dd : yyyy",
+        todayHighlight: true,
+        autoclose: true,
+        container: '#box1',
+        orientation: 'top right'
     });
+
+    $( '#datepicker2' ).datepicker({
+        format: 'mm \\ dd \\ yyyy',
+        todayHighlight: true,
+        autoclose: true,
+        container: '#box2',
+        orientation: 'top right'
+    });
+
+    $( '#datepicker1, #datepicker2, #simple, #datePicker' ).datepicker( 'setDate', today );
 
 });
