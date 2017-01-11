7599fd8d539a645a40410bd551f7712a4e3c0ab8
gearmobile <gearmobile@gmail.com>
2017-01-11 16:36:32 +0300

    test

--- test/js/app.js
+++ test/js/app.js
@@ -1,10 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
     //
     if ( localStorage ) {
-        var storedBookAuthor;
-        var storedBookTitle;
         var bookShelfButton = document.querySelector( '#bookshelf__button' );
         var bookShelfList = document.querySelector( '#bookShelfList' );
+        var bookShelfSecondary = document.querySelector( '#bookShelfSecondary' );
         //
         function saveData() {
             //
@@ -13,22 +12,61 @@ window.addEventListener('DOMContentLoaded', function () {
             //
             localStorage.setItem( 'author', bookAuthor.value );
             localStorage.setItem( 'title', bookTitle.value );
-            //
-            storedBookAuthor = localStorage.getItem( 'author' );
-            storedBookTitle = localStorage.getItem( 'title' );
-            //
+            // item & paragraphs --------------------
             var item = document.createElement( 'li' );
+            item.setAttribute( 'class', 'item-list' );
             var itemAuthor = document.createElement( 'p' );
+            itemAuthor.setAttribute( 'class', 'book-author' );
             var itemTitle = document.createElement( 'p' );
+            itemTitle.setAttribute( 'class', 'book-title' );
+            // buttons --------------------
+            var buttonRemove = document.createElement( 'button' );
+            var buttonEdit = document.createElement( 'button' );
+            buttonEdit.setAttribute( 'class', 'button-edit pull-left btn btn-primary' );
+            buttonRemove.setAttribute( 'class', 'button-remove pull-right btn btn-danger' );
+            var buttonRemoveText = document.createTextNode( 'Remove' );
+            var buttonEditText = document.createTextNode( 'Edit' );
+            buttonRemove.appendChild( buttonRemoveText );
+            buttonEdit.appendChild( buttonEditText );
+            // get local data --------------------
             itemAuthor.innerHTML = localStorage.getItem( 'author' );
             itemTitle.innerHTML = localStorage.getItem( 'title' );
+            // append to page --------------------
             item.appendChild( itemAuthor );
             item.appendChild( itemTitle );
+            item.appendChild( buttonEdit );
+            item.appendChild( buttonRemove );
             bookShelfList.appendChild( item );
         }
         //
         bookShelfButton.addEventListener( 'click', saveData, false );
+        //
+        bookShelfSecondary.addEventListener( 'click', function ( event ) {
+            if ( event && event.target.classList.contains( 'button-remove' ) ) {
+                var currentItem = event.target.parentNode;
+                currentItem.parentNode.removeChild( currentItem );
+            }
+        }, false );
     }
     //
 
 }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
