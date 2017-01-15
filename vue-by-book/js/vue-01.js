a82c83229a745314c38a4b95708273388eb317c1
gearmobile <gearmobile@gmail.com>
2017-01-15 14:50:24 +0300

    vue

---
+++ vue-by-book/js/vue-01.js
@@ -0,0 +1,10 @@
+
+var data = {
+    title: 'my shopping list',
+    fruits: [ 'apple', 'cherry', 'apricot', 'lemon', 'orange' ]
+};
+
+var wm = new Vue({
+    el: '#container',
+    data: data
+});
