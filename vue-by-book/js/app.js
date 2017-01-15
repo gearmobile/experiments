87413d6d4dae63a6678e0a6d10192b0394c490c2
gearmobile <gearmobile@gmail.com>
2017-01-15 14:15:29 +0300

    list to-do pure javascript

--- vue-by-book/js/app.js
+++ vue-by-book/js/app.js
@@ -1,30 +1,78 @@
 window.addEventListener( 'load', function () {
-    //
-    var input = document.querySelector( '#shoppingInput' );
-    var button = document.querySelector( '#shoppingButton' );
-    var list = document.querySelector( '#shoppingMain' );
-    //
-    button.addEventListener( 'click', function () {
-        var inputValue = input.value;
-        if ( inputValue === '' ) {
-            alert( 'Enter value!' );
-            return false;
-        } else {
-            var newDiv = document.createElement( 'div' );
-            var newLabel = document.createElement( 'label' );
-            var newLabelValue = document.createTextNode( inputValue );
-            var newInput = document.createElement( 'input' );
-            //
-            newDiv.setAttribute( 'class', 'form-check' );
-            newLabel.setAttribute( 'class', 'form-check-label' );
-            newInput.setAttribute( 'class', 'form-check-input' );
-            newInput.setAttribute( 'type', 'checkbox' );
-            newInput.setAttribute( 'value', '' );
-            //
-            newLabel.appendChild( newInput );
-            newLabel.appendChild( newLabelValue );
-            newDiv.appendChild( newLabel );
-            list.appendChild( newDiv );
+
+    // variables
+    // ------------------------------------------------
+    var list = document.querySelector( '#shoppingList' );
+
+    // generate close button for all of list items
+    // ------------------------------------------------
+    list.querySelectorAll( 'li' ).forEach( function ( item ) {
+        var span = document.createElement( 'span' );
+        var spanText = document.createTextNode( '\u00D7' );
+        span.appendChild( spanText );
+        span.setAttribute( 'class', 'close' );
+        item.appendChild( span );
+    });
+
+    // delete current item when it's close button pressed
+    // --------------------------------------------------
+    list.addEventListener( 'click', function ( event ) {
+        var targetElement = event.target;
+        if ( event && targetElement.classList.contains( 'close' ) ) {
+            var parent = targetElement.parentNode.parentNode;
+            parent.removeChild( targetElement.parentNode );
         }
     });
+
+    // add class checked to checked item of list
+    // ------------------------------------------------
+    list.addEventListener( 'click', function ( event ) {
+        var targetElement = event.target;
+        if ( event && targetElement.tagName === 'LI' ) {
+            targetElement.classList.toggle( 'checked' );
+        }
+    });
+
+    // generate new item of list when button add is pressed
+    // ------------------------------------------------
+    function createItemList() {
+        var inputValue = document.querySelector( '#shoppingInput' ).value;
+        if ( inputValue === '' ) {
+            alert( 'You must write something!' );
+            return false;
+        } else {
+            var item = document.createElement( 'li' );
+            var itemValue = document.createTextNode( inputValue );
+            var span = document.createElement( 'span' );
+            var spanText = document.createTextNode( '\u00D7' );
+            span.appendChild( spanText );
+            span.setAttribute( 'class', 'close' );
+            item.appendChild( span );
+            item.appendChild( itemValue );
+            list.appendChild( item );
+            document.querySelector( '#shoppingInput' ).value = '';
+        }
+    }
+
+    // catch submit on form
+    document.getElementById( 'shoppingForm' ).addEventListener( 'submit', function ( event ) {
+        event.preventDefault();
+        createItemList();
+    }, false );
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
