0de7b3025b1dca3cd3ad0fae229aef412cca8860
gearmobile <gearmobile@gmail.com>
2017-01-17 12:12:51 +0300

    gsap-banner finish

--- gsap-banner/js/app.js
+++ gsap-banner/js/app.js
@@ -1,4 +1,5 @@
 window.addEventListener( 'load', function () {
+
     // -------------------------------
     var text1 = document.getElementById( 'adText1' );
     var text2 = document.getElementById( 'adText2' );
@@ -12,14 +13,15 @@ window.addEventListener( 'load', function () {
     var fnEasing0 = Power0.easeOut;
     var fnEasing1 = Power2.easeOut;
     var fnEasing2 = Bounce.easeOut;
-    var fnEasing3 = Back.easeOut;
     var tl = new TimelineMax();
+
     // --------------------------------
     function textColor() {
         const tlTextColor = new TimelineMax();
         tlTextColor
             .to( icon, ( duration + 1 ), { fill: '#b22222' } );
     }
+
     // --------------------------------
     function fnRotation() {
         const tlWelcome = new TimelineMax({ onComplete: textColor, transformOrigin: 'center center' });
@@ -37,8 +39,10 @@ window.addEventListener( 'load', function () {
         .from( marker, duration, { y: -200, ease: fnEasing2 }, '-=2' )
         .from( shadow, duration, { autoAlpha: 0, ease: fnEasing2 }, '-=2' )
         .from( text3, duration, { scale: .2, opacity: 0 }, '-=1' )
-        .to( icon, duration+.5, { left: 525, ease: fnEasing1, onComplete: fnRotation }, '-=.6' );
+        .to( icon, ( duration + .5 ), { left: 525, ease: fnEasing1, onComplete: fnRotation }, '-=.6' );
+
     // --------------------------------
     var tl2 = new TimelineMax({ repeat: -1 });
     tl2.to( adBg, duration + 64, { backgroundPositionX: 649, ease: fnEasing0 } );
+    // --------------------------------
 }, false );
