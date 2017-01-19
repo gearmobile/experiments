71400b56b3b7340b42befd2a4e3da6ab4c285765
gearmobile <gearmobile@gmail.com>
2017-01-19 13:31:51 +0300

    vue

---
+++ vue.js/case-12/src/components/posts.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="blog">
+        <div class="post" v-for="item in posts">
+            <span class="post__id">{{ item.id }}</span>
+            <h3 class="post__title">{{ item.title }}</h3>
+            <p class="post__body">{{ item.body }}</p>
+        </div>
+    </section>
+</template>
+
