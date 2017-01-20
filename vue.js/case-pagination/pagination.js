bc159ca466861849ba312aed6854e9cdb7e46714
gearmobile <gearmobile@gmail.com>
2017-01-20 12:00:46 +0300

    vue pagination

--- vue.js/case-pagination/pagination.js
+++ vue.js/case-pagination/pagination.js
@@ -1,3 +1,22 @@
 Vue.component( 'pagination', {
-    template: '#pagination-template'
+    template: '#pagination-template',
+    props: {
+        current: {
+            type: Number,
+            default: 1
+        }
+    },
+    computed: {
+        getNextPage: function () {
+            return this.current + 1;
+        },
+        getPrevPage: function () {
+            return this.current - 1;
+        }
+    },
+    methods: {
+        changePage: function ( pageNumber ) {
+            this.$emit( 'page-change', pageNumber );
+        }
+    }
 });
