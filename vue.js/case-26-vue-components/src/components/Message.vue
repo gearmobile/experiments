fb3aa48ec02accc158ce411f4438b91f93bacee9
gearmobile <gearmobile@gmail.com>
2017-01-26 13:10:58 +0300

    vue components communication

---
+++ vue.js/case-26-vue-components/src/components/Message.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header">
+        <h1 v-text="message"></h1>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+      data () {
+        return {
+          message: 'Hello from Vue'
+        }
+      }
+  }
+</script>
+
+<style scoped>
+    .header {
+        text-align: center;
+        text-transform: uppercase;
+        font-size: 30px;
+        margin: 0 0 20px;
+    }
+</style>
