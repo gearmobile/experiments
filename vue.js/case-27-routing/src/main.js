ad1fa9ba9d5a3792b5bcfe7c7a94b24ebd969dc7
gearmobile <gearmobile@gmail.com>
2017-01-26 14:54:33 +0300

    vue router

--- vue.js/case-27-routing/src/main.js
+++ vue.js/case-27-routing/src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
+
+// INCLUDE VUE PLUGINS
+// ----------------------------------
 import VueRouter from 'vue-router'
 
-// LIST OF COMPONENTS
+// LIST OF VUE COMPONENTS
 // -----------------------------------
 import App from './App.vue'
 import Home from  './components/Home.vue'
 import Users from './components/Users.vue'
 
+// LIST OF USED VUE PLUGINS
+// -----------------------------------
 Vue.use( VueRouter );
 
+// NEW ROUTER
+// -----------------------------------
 const newRouter = new VueRouter({
   routes: [
     { path: '/users', component: Users },
@@ -16,6 +23,8 @@ const newRouter = new VueRouter({
   ]
 });
 
+// NEW VUE INSTANCE
+// -----------------------------------
 new Vue({
   el: '#app',
   router: newRouter,
