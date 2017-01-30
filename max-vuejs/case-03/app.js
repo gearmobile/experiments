c34c9944711d2dfe549d383aff9c210a4a5fb7e0
gearmobile <gearmobile@gmail.com>
2017-01-31 00:38:08 +0300

    max-vue

--- max-vuejs/case-03/app.js
+++ max-vuejs/case-03/app.js
@@ -1,5 +1,41 @@
 new Vue({
   data: {
     name: 'John',
+    isAttached: false,
+    color: 'red',
+    width: 100,
+    height: 100,
+    radius: 0,
+    square: 100
   },
+  // использование watchers
+  watch: {
+    name: function () {
+      const wm = this;
+      setInterval( function () {
+        wm.name = 'John';
+      }, 4000 );
+    }
+  },
+  computed: {
+    divClass: function () {
+      return {
+        green: this.isAttached,
+        blue: !this.isAttached
+      }
+    },
+    myStyle: function () {
+      return {
+        backgroundColor: this.color,
+        width: this.width + 'px',
+        height: this.height + 'px',
+        borderRadius: this.radius + 'px'
+      }
+    },
+    mySquare: function () {
+      return {
+        width: this.square + 'px'
+      }
+    }
+  }
 }).$mount('#app');
