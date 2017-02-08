b88303c881fa854ca2cedc4afca97fad7ad6b1f9
gearmobile <gearmobile@gmail.com>
2017-02-08 23:56:55 +0300

    vue-cplm

---
+++ vue-component/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <button @click="chgUserAgeUp">increment user age</button>
+    <button @click="chgUserAgeDown">decrement user age</button>
+    <!--<p @parentName="surname = $event"></p>-->
+    <hello :userName="name" @parentName="surname = $event"></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      age: 29,
+      name: 'john',
+      surname: 'aaronson',
+    };
+  },
+  components: {
+    Hello,
+  },
+  methods: {
+    chgUserAgeUp() {
+      this.age += 1;
+    },
+    chgUserAgeDown() {
+      this.age -= 1;
+    },
+  },
+};
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ vue-component/src/components/Hello.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <h3>{{ reverseName() }}</h3>
+    <button @click="chgParentName">create event</button>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+  props: {
+    userAge: {
+      type: Number,
+      default: 34,
+    },
+    userName: {
+      type: String,
+    },
+    parName: {
+      type: String,
+      default: 'johnson',
+    },
+  },
+  methods: {
+    reverseName() {
+      return this.userName.split('').reverse().join('');
+    },
+    chgParentName() {
+      this.$emit('parentName', this.parName);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
