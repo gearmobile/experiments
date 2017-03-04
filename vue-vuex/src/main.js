7bf54d8f0476a4156a0f872b530c61bce71cffb5
gearmobile <gearmobile@gmail.com>
2017-03-04 15:55:58 +0300

    vuex

--- vue-vuex/src/main.js
+++ vue-vuex/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue';
 import store from './store/store';
 
 new Vue({
-  store,
+  store, // указываем хранилище в опции "store", что обеспечит доступ к нему также и для всех дочерних компонентов
   render: h => h(App)
 }).$mount('#app');
