c771a05d38cc3341ff280aed50e00a9f01f816d6
gearmobile <gearmobile@gmail.com>
2017-04-20 15:18:50 +0300

    nuxt

---
+++ nuxt-basic-example/pages/about.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #about
+    h1.caption {{ title }}
+    nav.links
+      router-link.button.button--green( tag="a", to="/" ) go home
+</template>
+
+<script>
+  export default {
+    name: 'about',
+    data () {
+      return {
+        title: 'this is about page'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+// 
+</style>
---
+++ nuxt-basic-example/pages/contacts.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #contacts
+    h1.caption {{ title }}
+    nav.links
+      router-link.button.button--green( tag="a", to="/" ) go home
+</template>
+
+<script>
+  export default {
+    name: 'contacts',
+    data () {
+      return {
+        title: 'this is contacts page'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .caption {
+    text-align: center;
+    color: forestgreen;
+  }
+</style>
