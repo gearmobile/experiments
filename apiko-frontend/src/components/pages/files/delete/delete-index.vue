a218e17cdf18497012a363f9b6b3a44eafb8de05
gearmobile <gearmobile@gmail.com>
2017-05-12 15:29:03 +0300

    files section delete finished

---
+++ apiko-frontend/src/components/pages/files/delete/delete-index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  q-layout
+
+    // NAVIGATION SECTION
+    .toolbar( slot="header" )
+      q-toolbar-title( :padding="1" ) Files Delete Section
+    q-tabs( slot="navigation" )
+      q-tab( route="/files", exact, replace ) go back
+      q-tab( route="/files/delete/delete", exact, replace ) file delete
+    
+    // CONTAINER SECTION
+    .row.gutter.auto
+      .layout-view
+        .layout-padding
+
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
+
+<style lang="styl">
+  /**/
+</style>
