2ac85f3aa09e9ad4c8edab5a4e73575cec69307e
gearmobile <gearmobile@gmail.com>
2017-05-29 23:41:00 +0300

    add alert-component

---
+++ simple-alert-vuejs/src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #app
+    app-home
+</template>
+
+<script>
+  import Home from './components/home'
+
+  export default {
+    name: 'app',
+    components: {
+      appHome: Home
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin-top: 60px;
+  }
+</style>
---
+++ simple-alert-vuejs/src/components/home.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  #home
+    simplert
+      isUseRadius = true
+      isUseIcon = true
+      ref = 'simplert'
+    button( type="button", @click="openWindow()" ) alert info
+</template>
+
+<script>
+  import Simplert from 'vue2-simplert'
+
+  export default {
+    name: 'home',
+    data () {
+      return {
+        config: {
+          title: 'Opened Simplert',
+          message: 'Now you can close this window',
+          type: 'info',
+          onClose: this.onClose
+        }
+      }
+    },
+    components: {
+      Simplert
+    },
+    methods: {
+      openWindow () {
+        this.$refs.simplert.openSimplert(this.config)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /**/
+</style>
