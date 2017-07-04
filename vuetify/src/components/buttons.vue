0173ddc045973843750cf18f91a59e92f6c0dda8
gearmobile <gearmobile@gmail.com>
2017-07-04 12:27:40 +0300

    vuetify - button component

--- vuetify/src/components/buttons.vue
+++ vuetify/src/components/buttons.vue
@@ -105,6 +105,57 @@
               | Editable
             v-btn-dropdown( :options="dropdown_edit", max-height="auto", editable="editable" )
 
+    h4.text-xs-right.my-4
+      | #5 Button Toggle
+
+    v-layout( mb-3 )
+
+      v-flex( xs6 )
+        v-card
+          v-card-text
+            p
+              | Exclusive
+            v-btn-toggle( :options="toggle_options", v-model="toggle_exclusive" )
+
+      v-flex( xs6 )
+        v-card
+          v-card-text
+            p
+              | Multiple
+            v-btn-toggle( :options="toggle_options_multiple", v-model="toggle_multiple", multiple )
+
+    v-layout( mb-3 )
+
+      v-flex( xs6 )
+        v-card
+          v-card-text
+            p
+              | No Options Selected
+            v-btn-toggle( :options="toggle_options", v-model="toggle_none" )
+
+      v-flex( xs6 )
+        v-card
+          v-card-text
+            p
+              | One Option Selected
+            v-btn-toggle( :options="toggle_options_multiple", v-model="toggle_one", multiple )
+
+    v-layout
+
+      v-flex( xs6 )
+        v-card
+          v-card-text
+            p
+              | Text Options
+            v-btn-toggle( :options="toggle_text", v-model="text" )
+
+      v-flex( xs6 )
+        v-card
+          v-card-text
+            p
+              | Text &amp; Icon Options
+            v-btn-toggle( :options="toggle_text_icon", v-model="icon" )
+
 </template>
 
 <script>
@@ -140,6 +191,42 @@
           { text: '50%' },
           { text: '25%' },
           { text: '0%' }
+        ],
+        // TOGGLE EXCLUSIVE
+        toggle_exclusive: 1,
+        toggle_options: [
+          { icon: 'format_align_left', value: 1 },
+          { icon: 'format_align_center', value: 2 },
+          { icon: 'format_align_right', value: 3 },
+          { icon: 'format_align_justify', value: 4 }
+        ],
+        // TOGGLE MULTIPLE
+        toggle_multiple: [1, 3],
+        toggle_options_multiple: [
+          { icon: 'format_bold', value: 1 },
+          { icon: 'format_italic', value: 2 },
+          { icon: 'format_underlined', value: 3 },
+          { icon: 'format_color_fill', value: 4 }
+        ],
+        // TOGGLE NONE
+        toggle_none: null,
+        // TOOGLE ONE
+        toggle_one: [1],
+        // TOGGLE TEXT
+        text: 3,
+        toggle_text: [
+          { text: 'Left', value: 1 },
+          { text: 'Center', value: 2 },
+          { text: 'Right', value: 3 },
+          { text: 'Justify', value: 4 }
+        ],
+        // TOGGLE TEXT AND ICON
+        icon: null,
+        toggle_text_icon: [
+          { text: 'Left', icon: 'format_align_left', value: 1 },
+          { text: 'Center', icon: 'format_align_center', value: 2 },
+          { text: 'Right', icon: 'format_align_right', value: 3 },
+          { text: 'Justify', icon: 'format_align_justify', value: 4 }
         ]
       }
     }
