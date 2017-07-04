c21c5ee874ec5e4edc64f306f3279f9f58cd2d3d
gearmobile <gearmobile@gmail.com>
2017-07-04 09:27:19 +0300

    vuetify

--- vuetify/src/App.vue
+++ vuetify/src/App.vue
@@ -2,36 +2,17 @@
 
   v-app
   
-    grid
-    alert
-    breadcrumbs
-    lists
-    cards
-    buttons
+    app-grid
+
       
 </template>
 
 <script>
   import grid from './components/grid.vue'
-  import alert from './components/alert.vue'
-  import breadcrumbs from './components/breadcrumbs.vue'
-  import lists from './components/lists.vue'
-  import cards from './components/card.vue'
-  import buttons from './components/button.vue'
 
   export default {
-    data () {
-      return {
-        title: 'Vuetify.js'
-      }
-    },
     components: {
-      grid,
-      alert,
-      breadcrumbs,
-      lists,
-      cards,
-      buttons
+      appGrid: grid
     }
   }
 </script>
