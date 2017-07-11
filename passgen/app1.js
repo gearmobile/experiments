5d1fcadc475c548573cd5f10fc48d61d80facf3a
gearmobile <gearmobile@gmail.com>
2017-07-11 10:15:18 +0300

    passgen

--- passgen/app1.js
+++ passgen/app1.js
@@ -2,15 +2,25 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
 
   const inputOne = document.querySelector('#primoResult')
   const buttonOne = document.querySelector('#primo')
+  const buttonTwo = document.querySelector('#secondo')
+
   const pattern = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   const passLength = 10
   let password = ''
 
   function passgen () {
-    password += pattern.charAt(Math.floor(Math.random() * pattern.length))
+    for (let i = 0; i < passLength; i += 1) {
+      password += pattern.charAt(Math.floor(Math.random() * pattern.length))
+    }
     inputOne.value = password
+    password = ''
+  }
+
+  function resetInput () {
+    inputOne.value = ''
   }
 
   buttonOne.addEventListener('click', passgen, false)
+  buttonTwo.addEventListener('click', resetInput, false)
 
 });
