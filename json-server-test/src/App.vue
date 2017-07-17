5f61ebe30bed60a17872d8d86fc24e9711a952e7
gearmobile <gearmobile@gmail.com>
2017-07-17 11:25:44 +0300

    test jsonserver by jquery

---
+++ json-server-test/src/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  v-app
+    v-container( fluid )
+      
+      // PRIMO SECTION
+      v-layout( row )
+        v-flex( xs12 )
+          app-primo
+
+      // SECONDO SECTION
+      v-layout( row )
+        v-flex( xs12 )
+          app-secondo
+
+</template>
+
+<script>
+  import primo from './components/primo.vue'
+  import secondo from './components/secondo.vue'
+
+  export default {
+    data () {
+      return {
+        //
+      }
+    },
+    components: {
+      appPrimo: primo,
+      appSecondo: secondo
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ json-server-test/src/components/primo.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  v-card
+    v-card-title
+      h3.headline.mb-0
+        | jQuery methods
+    v-card-text
+      //
+    v-card-actions
+      v-btn.teal( flat, @click.native="onGet()" )
+        | get resource
+      v-btn.teal( flat, @click.native="onPost()" )
+        | post resource
+</template>
+
+<script>
+
+  import $ from 'jquery'
+  const root = 'http://localhost:3000'
+
+  export default {
+    name: 'primo',
+    methods: {
+      onGet () {
+        $.ajax(root + '/posts', {
+          method: 'GET'
+        }).then(data => {
+          console.log(data)
+        })
+      },
+      onPost () {
+        const newPost = {
+          id: 101,
+          userId: 10,
+          title: 'new post from jquery',
+          body: 'this post created by jquery method'
+        }
+        $.ajax(root + '/posts', {
+          method: 'POST',
+          data: newPost
+        }).then(data => {
+          console.log(data)
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  //
+</style>
+
