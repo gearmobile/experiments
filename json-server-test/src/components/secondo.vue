888d85997442b30bab7c5e91435dfc37bcaad83b
gearmobile <gearmobile@gmail.com>
2017-07-17 14:36:12 +0300

    json-server-test - fix testing by axios

--- json-server-test/src/components/secondo.vue
+++ json-server-test/src/components/secondo.vue
@@ -11,10 +11,12 @@
         | get resource
       v-btn.white--text( flat, @click.native="onPost()" )
         | post resource
-      v-btn.white--text( flat, @click.native="onDelete(2)" )
+      v-btn.white--text( flat, @click.native="onDelete(punto)" )
         | delete resource
-      v-btn.white--text( flat, @click.native="onFilter(4)" )
+      v-btn.white--text( flat, @click.native="onFilter(punto)" )
         | filter resource
+      v-spacer
+      v-select( :items="puntos", label="Select ID here ...", item-text="name", item-value="status", v-model="punto" )
 </template>
 
 <script>
@@ -23,19 +25,67 @@
 
   export default {
     name: 'secondo',
+    data () {
+      return {
+        length: 100,
+        puntos: [],
+        punto: null
+      }
+    },
     methods: {
       onGet () {
-        //
+        axios.get(root + '/albums')
+          .then(data => {
+            console.log(data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
       },
       onPost () {
-        //
+        const newPost = {
+          "userId": 11,
+          "id": 101,
+          "title": "added by axios"
+        }
+        axios.post(root + '/albums', newPost)
+          .then(data => {
+            console.log(data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
       },
-      onDelete () {
-        //
+      onDelete (value) {
+        axios.delete(root + '/albums/' + value)
+          .then(response => {
+            console.log(response.status)
+            console.log(response.statusText)
+          })
+          .catch(error => {
+            console.log(error)
+          })
       },
-      onFilter () {
-        //
+      onFilter (value) {
+        axios.get(root + '/albums/?userId=' + value)
+          .then(data => {
+            console.log(data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+      initArray () {
+        for (let i = 1; i < this.length; i += 1) {
+          this.puntos.push({
+            name: i,
+            status: i
+          })
+        }
       }
+    },
+    created () {
+      this.initArray()
     }
   }
 </script>
