ba5565ac813b6235168360ac434618c019a52c68
gearmobile <gearmobile@gmail.com>
2017-09-07 00:32:26 +0300

    challenge

---
+++ max-challenges/src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  
+  v-app( light )
+    v-container
+      v-layout( row )
+        v-flex( xs12 )
+          router-view
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        //
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ max-challenges/src/components/Home.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  v-container
+    v-layout( row )
+      v-flex( xs12 )
+        p
+          | Vue.js is awesome - {{ name }} ({{ age }})
+        p
+          | Multiplied age: {{ triple }}
+        p
+          | Random value: {{ getRandom }}
+        p
+          img( :src="path", :alt="image.title" )
+        p
+          input( type="text" :value="name" )
+</template>
+
+<script>
+  function random (minVal, maxVal) {
+    const min = Math.ceil(minVal)
+    const max = Math.floor(maxVal)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+  export default {
+    name: 'home',
+    data () {
+      return {
+        name: 'Valery Semenencko',
+        age: 32,
+        image: {
+          path: 'assets/vuejs-logo.jpg',
+          title: 'Image'
+        }
+      }
+    },
+    computed: {
+      triple () {
+        return this.age * 3
+      },
+      getRandom () {
+        return random(0, 10)
+      },
+      path () {
+        return require(this.image.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /**/
+</style>
