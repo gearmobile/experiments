f28b0c19ebd434265460708b8503b60f20d81cb7
gearmobile <gearmobile@gmail.com>
2017-09-11 14:47:05 +0300

    nasa

--- nasa/src/components/Home.vue
+++ nasa/src/components/Home.vue
@@ -1,44 +1,80 @@
 <template lang="pug">
 
-  // https://api.nasa.gov/api.html#apod
+  v-container
 
-  v-layout( row )
-    v-flex( xs12 )
-      h4
-        | APOD (Astronomy Picture of the Day)
-      v-card
-        v-card-media.white--text( :src="image.url", style="height: 400px" )
-          v-container( fill-height, fluid )
-            v-layout( fill-height )
-              v-flex( xs12, align-end, flexbox )
-                span.headline
-                  | {{ image.title }}
-        v-card-title
-          article
-            p.grey--text
-              | {{ image.date }}
-            p
-              | {{ image.explanation }}
+    // https://api.nasa.gov/api.html#apod
+
+    v-layout( row )
+      v-flex( xs12 )
+        h2
+          | search images
+        form( @submit.prevent="onSearch(query)" )
+          v-text-field( v-model="query", label="Input term for search" )
+
+    v-layout( row )
+      v-flex( xs12 )
+        v-card.mb-2( v-for="item in collection", :key="item" )
+          v-card-media( :src="item.links[0].href", height="200px" )
+          v-card-title( primary-title )
+            article
+              h3.headline.mb-0
+                | {{ item.data[0].title }}
+              p.mb-0
+                | {{ item.data[0].description }}
+              //- p.mb-0
+              //-   | {{ item.data[0].date_created }}
+
+    v-layout( row )
+      v-flex( xs12 )
+        h4
+          | APOD (Astronomy Picture of the Day)
+        v-card
+          v-card-media.white--text( :src="image.url", style="height: 400px" )
+            v-container( fill-height, fluid )
+              v-layout( fill-height )
+                v-flex( xs12, align-end, flexbox )
+                  span.headline
+                    | {{ image.title }}
+          v-card-title
+            article
+              p.grey--text
+                | {{ image.date }}
+              p
+                | {{ image.explanation }}
 
 </template>
 
 <script>
   import axios from 'axios'
+  const apod = 'https://api.nasa.gov/planetary/apod'
+  const images = 'https://images-api.nasa.gov/'
   const key = 'E63VawPtEeRx6v2Mkzh2HqyeggvHL9dFPtctVcaN'
 
   export default {
     name: 'home',
     data () {
       return {
-        image: null
+        image: null,
+        query: null,
+        collection: []
       }
     },
     methods: {
       getImage () {
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=' + key)
+        axios.get(apod + '?api_key=' + key)
           .then(response => {
             console.log(response.data)
             this.image = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+      onSearch (value) {
+        axios.get(images + 'search?q=' + value + '&media_type=image')
+          .then(response => {
+            console.log(response.data.collection.items)
+            this.collection = response.data.collection.items.splice(0, 10)
           })
           .catch(error => {
             console.log(error)
