269566ca0f9a6195a2ad8eaa67e9d02cd0a7dd52
gearmobile <gearmobile@gmail.com>
2017-09-19 21:20:51 +0300

    bulma - figure out bulma layout and how to customize variables

--- bulma-exp/src/App.vue
+++ bulma-exp/src/App.vue
@@ -1,18 +1,15 @@
 <template lang="pug">
   #app
     app-home
-    //- app-secondo
 </template>
 
 <script>
   const Home = () => import('./components/Home.vue')
-  // const Secondo = () => import('./components/Secondo.vue')
 
   export default {
     name: 'app',
     components: {
       appHome: Home
-      // appSecondo: Secondo
     }
   }
 </script>
--- bulma-exp/src/components/Home.vue
+++ bulma-exp/src/components/Home.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
 
   .container
+
     .columns
       .column( v-for="item in items", :key="item" )
         .notification.has-text-centered.is-primary
           p.title
             | {{ item }}
+    
+    .columns.is-centered
+      .column.is-8
+        .notification.is-success
+          p.subtitle.has-text-centered.is-capitalized
+            | centered
 
 </template>
 
